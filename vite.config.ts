5c87c71990e56e678866771dedaf584b111a6938
Zarkec <2656207830@qq.com>
2026-06-04 13:19:17 +0800

    ci: 修复 GitHub Pages workflow 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'github-pages' ? '/alpha-z/' : undefined,
+export default defineConfig({
   plugins: [react()],
   server: {
     host: '127.0.0.1',
     port: 5174,
   },
-}))
+})
