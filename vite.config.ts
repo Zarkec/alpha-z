7c3613797b10906f1bb58dbec62a520bc348b028
Zarkec <2656207830@qq.com>
2026-06-04 13:19:17 +0800

    ci: 配置 GitHub Pages 部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github-pages' ? '/alpha-z/' : undefined,
   plugins: [react()],
   server: {
     host: '127.0.0.1',
     port: 5174,
   },
-})
+}))
